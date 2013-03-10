d5f6570b9472ca7db0185a49ae5e3f7ec3988fdc
fazioa <fazioa@dec8a6de-b98e-2a6d-bf8a-da8dc4ae4f73>
2013-03-10 22:53:05 +0000

    ver 2.79.8 Corretto ulteriore bug importazione dati Corretto bug che impediva di eseguire due controlli separatamente ed in contemporanea

--- xOrdineServizio/My Project/AssemblyInfo.vb
+++ xOrdineServizio/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' utilizzando l'asterisco (*) come descritto di seguito:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.79.7.0")> 
-<Assembly: AssemblyFileVersion("3.8")> 
+<Assembly: AssemblyVersion("2.79.8.0")> 
+<Assembly: AssemblyFileVersion("3.9")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("it")> 
