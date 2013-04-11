c1615ca896c518af4359f9ddd7c447f4776e865a
fazioa <fazioa@dec8a6de-b98e-2a6d-bf8a-da8dc4ae4f73>
2013-04-11 23:46:51 +0000

    2.79.13 Inserito form Contatti...

---
+++ xOrdineServizio/FContatti.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FContatti
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBoxNome = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxMessaggio = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnInvia = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'TextBoxNome
+        '
+        Me.TextBoxNome.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxNome.Location = New System.Drawing.Point(138, 47)
+        Me.TextBoxNome.Name = "TextBoxNome"
+        Me.TextBoxNome.Size = New System.Drawing.Size(388, 20)
+        Me.TextBoxNome.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(120, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Nome ed indirizzo email:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Messaggio:"
+        '
+        'TextBoxMessaggio
+        '
+        Me.TextBoxMessaggio.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxMessaggio.Location = New System.Drawing.Point(79, 76)
+        Me.TextBoxMessaggio.Multiline = True
+        Me.TextBoxMessaggio.Name = "TextBoxMessaggio"
+        Me.TextBoxMessaggio.Size = New System.Drawing.Size(447, 155)
+        Me.TextBoxMessaggio.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.Label3.Location = New System.Drawing.Point(11, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(520, 24)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Invia un commento, una proposta, oppure segnala un errore."
+        '
+        'btnInvia
+        '
+        Me.btnInvia.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnInvia.AutoSize = True
+        Me.btnInvia.Enabled = False
+        Me.btnInvia.Location = New System.Drawing.Point(334, 237)
+        Me.btnInvia.Name = "btnInvia"
+        Me.btnInvia.Size = New System.Drawing.Size(192, 23)
+        Me.btnInvia.TabIndex = 5
+        Me.btnInvia.Text = "Invia (Solo con connessione internet)"
+        Me.btnInvia.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 3000
+        '
+        'FContatti
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(538, 272)
+        Me.Controls.Add(Me.btnInvia)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBoxMessaggio)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBoxNome)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FContatti"
+        Me.ShowIcon = False
+        Me.Text = "Contatti"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBoxNome As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxMessaggio As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnInvia As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+End Class
