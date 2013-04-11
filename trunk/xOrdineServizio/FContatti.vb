213b383cae451225bede084be50eb9fa6b491e53
fazioa <fazioa@dec8a6de-b98e-2a6d-bf8a-da8dc4ae4f73>
2013-04-11 23:46:51 +0000

    2.79.13 Inserito form Contatti...

---
+++ trunk/xOrdineServizio/FContatti.vb
@@ -0,0 +1,44 @@
+﻿Public Class FContatti
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        If (ActionsLibrary.ConnessioneInternet()) Then
+            abilitaCampi(True)
+        Else
+            abilitaCampi(False)
+        End If
+
+    End Sub
+
+    Sub abilitaCampi(bEnabled As Boolean)
+        
+        If (bEnabled) Then
+            btnInvia.Text = "Invia"
+            btnInvia.Enabled = True
+
+        Else
+            btnInvia.Text = "Invio possib.solo con conness.internet attiva"
+            btnInvia.Enabled = False
+
+        End If
+    End Sub
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+        Timer1.Start()
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Private Sub btnInvia_Click(sender As System.Object, e As System.EventArgs) Handles btnInvia.Click
+        Dim cats() As String = {"Messaggi Utenti"}
+        Dim sTitoloConComando As String = My.Settings.comando & " " & My.Settings.comandoSecondaRiga & " - " & TextBoxNome.Text.Trim
+        Dim result As Integer = ActionsLibrary.newBlogPostSend(sTitoloConComando, TextBoxMessaggio.Text.Trim, cats)
+        If result > 0 Then
+            MessageBox.Show("Inviato", "", MessageBoxButtons.OK, MessageBoxIcon.None)
+            TextBoxNome.Text = ""
+            TextBoxMessaggio.Text = ""
+        End If
+    End Sub
+End Class
---
+++ trunk/xOrdineServizio/dbAlegatoADataSet.vb
@@ -0,0 +1,2 @@
+﻿Partial Class dbAlegatoADataSet
+End Class
